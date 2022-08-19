d0f684998a8cec618d21a339c50a1702b6d0a7a5
joaoprds <joaopaulorochadesousa@gmail.com>
2022-08-19 18:52:12 -0300

    feat: Init project

---
+++ infrastructure/grpc/server/server.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"github.com/joaoprds/project_bank/infrastructure/grpc/pb"
+	"github.com/joaoprds/project_bank/infrastructure/grpc/service"
+	"github.com/joaoprds/project_bank/usecase"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+)
+
+type GRPCServer struct {
+	ProcessTransactionUseCase usecase.UseCaseTransaction
+}
+
+func NewGRPCServer() GRPCServer {
+	return GRPCServer{}
+}
+
+func (g GRPCServer) Serve() {
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("could not listen tpc port")
+	}
+	transactionService := service.NewTransactionService()
+	transactionService.ProcessTransactionUseCase = g.ProcessTransactionUseCase
+	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+	pb.RegisterPaymentServiceServer(grpcServer, transactionService)
+	grpcServer.Serve(lis)
+}
